5c54eadba3ef0a64a9ac2c5172be08fd0f7944c0
Antti Keränen <detegr@gmail.com>
2015-04-29 18:10:02 +0300

    Ctrl-c handler + channels

--- src/bot.rs
+++ src/bot.rs
@@ -2,15 +2,19 @@ use std::net::{TcpStream};
 use std::io::{BufStream,BufRead,Write};
 use std::io::Result;
 use std::thread;
+use std::sync::mpsc::channel;
 use parser;
 use parser::Command;
+use std::sync::atomic::Ordering;
+use std::thread::JoinGuard;
+use std::net::Shutdown;
 
-pub fn start(server: &str, port: u16) -> Result<()>
+pub fn start(server: &str, port: u16) -> Result<(JoinGuard<()>,JoinGuard<()>)>
 {
+    let (tx, rx) = channel();
     let stream = BufStream::new(try!(TcpStream::connect((server, port))));
     let mut wstream = try!(stream.get_ref().try_clone());
-    let mut lwstream = try!(stream.get_ref().try_clone()); // TODO: Use channel instead
-    let listen_thread = thread::scoped(move || {
+    let reader_thread = thread::scoped(move || {
         for full_line in stream.lines() {
             for line in full_line.unwrap().split("\r\n") {
                 let mut parsed = parser::parse_message(line.as_ref()).unwrap();
@@ -18,13 +22,21 @@ pub fn start(server: &str, port: u16) -> Result<()>
                 println!("{:?}", parsed);
                 if parsed.command == Command::Named("PING") {
                     parsed.command = Command::Named("PONG");
-                    lwstream.write(format!("{}\r\n", parsed).as_bytes()).unwrap();
+                    tx.send(format!("{}\r\n", parsed)).unwrap();
                 }
             }
         }
     });
-    wstream.write(b"NICK RustBot\r\n").unwrap();
-    wstream.write(b"USER RustBot 0 * :RustBot\r\n").unwrap();
-    listen_thread.join();
-    Ok(())
+    let writer_thread = thread::scoped(move || {
+        wstream.write(b"NICK RustBot\r\n").unwrap();
+        wstream.write(b"USER RustBot 0 * :RustBot\r\n").unwrap();
+        while super::RUNNING.load(Ordering::SeqCst) {
+            match rx.try_recv() {
+                Ok(line) => {wstream.write(line.as_bytes()).unwrap();}
+                Err(_) => {thread::sleep_ms(500);}
+            };
+        }
+        wstream.shutdown(Shutdown::Both).unwrap();
+    });
+    Ok((writer_thread, reader_thread))
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@
 #[macro_use]
 
 extern crate nom;
+extern crate ctrlc;
 
 mod bot;
 mod parser;
 
+use std::sync::atomic::{ATOMIC_BOOL_INIT, AtomicBool, Ordering};
+use ctrlc::CtrlC;
+
+static RUNNING: AtomicBool = ATOMIC_BOOL_INIT;
+
 fn main() {
-    match bot::start("irc.quakenet.org", 6667) {
-        Ok(_) => {},
-        Err(e) => println!("{}", e)
+    RUNNING.store(true, Ordering::SeqCst);
+    CtrlC::set_handler(move || {
+        RUNNING.store(false, Ordering::SeqCst);
+    });
+    let (w,r) = match bot::start("irc.quakenet.org", 6667) {
+        Ok((w,r)) => (w,r),
+        Err(e) => panic!("{}", e)
+    };
+    while RUNNING.load(Ordering::SeqCst) {
     }
+    w.join();
+    r.join();
 }
